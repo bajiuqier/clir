e0ca36055f5df651336010ab2ab1b09a675f45c3
bajiuqier <yangher@foxmail.com>
2024-05-25 21:15:08 +0800

    coding

--- didid.py
+++ didid.py
@@ -2,14 +2,13 @@ import torch
 from transformers import AutoModel, AutoTokenizer
 from pathlib import Path
 
-HOME_DIR = Path.home().parent
+HOME_DIR = Path.home() / 'Desktop' / 'clir'
 
 # 指定保存模型的目录
-cache_directory = "/path/to/save/model"
-cache_directory = str(HOME_DIR / 'models' / 'xlm-roberta-base')
+model_path = str(HOME_DIR / 'models' / 'models--xlm-roberta-base')
 
 # 加载并保存模型和分词器到指定目录
-model = AutoModel.from_pretrained("xlm-roberta-base", trust_remote_code=True, cache_dir=cache_directory)
-tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base", trust_remote_code=True, cache_dir=cache_directory)
+model = AutoModel.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 print(model)
